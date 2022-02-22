2eb484b93a066c8c4bcbf8942b34d759e02810a9
Joseph Sebastian <josephsv96@gmail.com>
2022-02-22 10:30:48 +0100

    updated nvim config

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,6 @@
 vim.cmd [[
 try
-  colorscheme dracula 
+  colorscheme dracula
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -12,10 +12,10 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		-- formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-		diagnostics.flake8,
-		diagnostics.codespell,
-		diagnostics.cppcheck,
+	-- diagnostics.flake8,
+	-- diagnostics.codespell,
+	-- diagnostics.cppcheck,
 	},
 })
