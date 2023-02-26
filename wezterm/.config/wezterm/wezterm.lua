b299f14dcf73e9c6d30d83be52e58e134fffce03
Joseph Sebastian <josephsv96@gmail.com>
2023-02-26 09:09:17 +0100

    added wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+
+return {
+	font_size = 16.0,
+	font = wezterm.font("CaskaydiaCove Nerd Font"),
+	color_scheme = "Dracula",
+	window_background_opacity = 0.85,
+}
