8a5d4278326985860ebad1140539d9becb2e2b48
Joseph Sebastian <josephsv96@gmail.com>
2023-03-11 23:39:49 +0100

    removed wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require("wezterm")
-
-return {
-	font_size = 16.0,
-	font = wezterm.font("CaskaydiaCove Nerd Font"),
-	color_scheme = "Dracula",
-	window_background_opacity = 0.85,
-}
