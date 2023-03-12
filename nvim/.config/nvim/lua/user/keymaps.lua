9218da97e0778149d8ec936a9f5e1376cd55ed0e
Joseph Sebastian <josephsv96@gmail.com>
2023-03-12 09:35:17 +0100

    removed Alt+[j,k] move line in INPUT mode

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -33,8 +33,6 @@ map("n", "<S-h>", ":bprevious<CR>", { desc = "Next buffer" })
 -- move line up and down
 map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
 map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
 map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
