b2db337b40b19f66ccc2f55be797ecb36ddf3c04
Joseph Sebastian <josephsv96@gmail.com>
2024-01-21 13:30:56 +0100

    added fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,40 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# add user specific path
+fish_add_path $HOME/.local/bin/
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+# pnpm
+set -gx PNPM_HOME "/home/joseph/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+fish_add_path $PYENV_ROOT/bin
+pyenv init - | source
+
+# golang
+fish_add_path $HOME/.local/go/bin
+
+# abbreviations
+abbr -a -- vim nvim
+abbr -a -- ll ls -la 
+
+# set nvim as default editor
+set -gx EDITOR nvim
+
+# place at end
+starship init fish | source
+# end
