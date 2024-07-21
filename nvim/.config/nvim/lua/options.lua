539d3c490e0864d168f10d4acce181f5f4ca15f7
Joseph Sebastian <josephsv96@gmail.com>
2024-07-21 10:35:54 +0200

    nvim config rewrite part 3
    - completed revising neovim options

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,67 +1,69 @@
 --[[
 -- Options
 --]]
-
 local opt = vim.opt
 
--- previews substitutions in a split window
-opt.inccommand = "split"
-
--- ignore case in searching, but override when uppercase is used
-opt.smartcase = true
-opt.ignorecase = true
-
-opt.hlsearch = false
-
 opt.number = true
--- opt.relativenumber = true
+opt.relativenumber = true
 
 opt.mouse = "a"
+
+opt.showmode = false
 
 opt.clipboard = "unnamedplus"
 
 opt.breakindent = true
 
+opt.swapfile = false
+opt.backup = false
 opt.undofile = true
+
+-- case-insensitive searching, unless uppercase is used
+opt.ignorecase = true
+opt.smartcase = true
 
 opt.signcolumn = "yes"
 
--- decrease update time
+-- decrease update time and mapped sequence wait time (displays which-key sooner)
 opt.updatetime = 250
-opt.timeout = true
 opt.timeoutlen = 300
 
-opt.completeopt = 'menuone,noselect'
+opt.splitbelow = true
+opt.splitright = true
 
-opt.termguicolors = true
+-- how neovim displays whitespace characters
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
-opt.smartindent = true
+-- substitutions are previewed in a split window
+opt.inccommand = "split"
 
--- prevents creating a swapfile
-opt.swapfile = false
-opt.backup = false
+opt.cursorline = true
 
--- insert 2 spaces for each indentation or tab
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 
-opt.cursorline = true
+opt.wrap = true
+opt.linebreak = true
 
--- turn off text wrapping
 -- number of lines to show vertically/horizontally when scrolling
-opt.wrap = false
-opt.scrolloff = 8
+opt.scrolloff = 10
 opt.sidescrolloff = 8
+
+opt.smartindent = true
+
+opt.termguicolors = true
+
+opt.hlsearch = false
+
+
+
+-- opt.completeopt = 'menuone,noselect'
 
 -- opt.cmdheight = 1               -- more space in the neovim command line for displaying messages
 -- opt.pumheight = 10              -- pop up menu height
 --
--- opt.showmode = false            -- we don't need to see things like -- INSERT -- anymore
--- 
--- opt.splitbelow = true           -- force all horizontal splits to go below current window
--- opt.splitright = true           -- force all vertical splits to go to the right of current window
--- 
 -- opt.guifont = "monospace:h17"   -- the font used in graphical neovim applications
 
