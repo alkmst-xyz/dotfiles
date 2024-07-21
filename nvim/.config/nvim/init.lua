b29c58626c6c2c9614d941eb852d24bbd5e84261
Joseph Sebastian <josephsv96@gmail.com>
2024-07-21 10:02:08 +0200

    nvim config rewrite part 2

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,12 @@
+--[[
+-- Config init
+--]]
+ 
+-- Set <space> as the leader key
+-- NOTE: This should be set before loading lazy.nvim and plugins.
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 require("options")
 require("keymaps")
 require("lazy_init")
--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -2,76 +2,66 @@
 -- Options
 --]]
 
--- set highlight on search
-vim.o.hlsearch = false
+local opt = vim.opt
 
--- make line numbers default
-vim.wo.number = true
-
--- enable mouse mode
-vim.o.mouse = "a"
-
--- sync clipboard between OS and neovim
-vim.o.clipboard = "unnamedplus"
-
--- enable break indent
-vim.o.breakindent = true
-
--- enable persistent undo
-vim.o.undofile = true
+-- previews substitutions in a split window
+opt.inccommand = "split"
 
 -- ignore case in searching, but override when uppercase is used
-vim.o.ignorecase = true
-vim.o.smartcase = true
+opt.smartcase = true
+opt.ignorecase = true
 
--- always show signcolumn
-vim.o.signcolumn = "yes"
+opt.hlsearch = false
+
+opt.number = true
+-- opt.relativenumber = true
+
+opt.mouse = "a"
+
+opt.clipboard = "unnamedplus"
+
+opt.breakindent = true
+
+opt.undofile = true
+
+opt.signcolumn = "yes"
 
 -- decrease update time
-vim.o.updatetime = 250
-vim.o.timeout = true
-vim.o.timeoutlen = 300
+opt.updatetime = 250
+opt.timeout = true
+opt.timeoutlen = 300
 
--- better completion experience
-vim.o.completeopt = 'menuone,noselect'
+opt.completeopt = 'menuone,noselect'
 
--- most terminals support this
-vim.o.termguicolors = true
+opt.termguicolors = true
 
--- for smart indenting
-vim.o.smartindent = true
+opt.smartindent = true
 
 -- prevents creating a swapfile
-vim.o.swapfile = false
-vim.o.backup = false
-
+opt.swapfile = false
+opt.backup = false
 
 -- insert 2 spaces for each indentation or tab
--- convert tab to spaces
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
 
--- highlight the current line
-vim.o.cursorline = true
+opt.cursorline = true
 
 -- turn off text wrapping
 -- number of lines to show vertically/horizontally when scrolling
-vim.o.wrap = false
-vim.o.scrolloff = 8
-vim.o.sidescrolloff = 8
+opt.wrap = false
+opt.scrolloff = 8
+opt.sidescrolloff = 8
 
+-- opt.cmdheight = 1               -- more space in the neovim command line for displaying messages
+-- opt.pumheight = 10              -- pop up menu height
+--
+-- opt.showmode = false            -- we don't need to see things like -- INSERT -- anymore
+-- 
+-- opt.splitbelow = true           -- force all horizontal splits to go below current window
+-- opt.splitright = true           -- force all vertical splits to go to the right of current window
+-- 
+-- opt.guifont = "monospace:h17"   -- the font used in graphical neovim applications
 
--- -- still considering
--- cmdheight = 1, -- more space in the neovim command line for displaying messages
--- pumheight = 10, -- pop up menu height
-
--- showmode = false, -- we don't need to see things like -- INSERT -- anymore
-
--- splitbelow = true, -- force all horizontal splits to go below current window
--- splitright = true, -- force all vertical splits to go to the right of current window
-
--- relativenumber = false, -- set relative numbered lines
-
--- guifont = "monospace:h17", -- the font used in graphical neovim applications
