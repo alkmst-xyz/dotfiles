f99c05c0589c5f9bc0742696f4111e0f5846f9b1
Joseph Sebastian <josephsv96@gmail.com>
2024-07-21 15:11:29 +0200

    nvim config rewrite part 11: todo-comments

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -316,4 +316,22 @@ return {
             },
         },
     },
+
+
+    -- Highlight and search for todo comments
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        cmd = { "TodoTrouble", "TodoTelescope" },
+        event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+        opts = {},
+        keys = {
+            { "]t",         function() require("todo-comments").jump_next() end,              desc = "Next Todo Comment" },
+            { "[t",         function() require("todo-comments").jump_prev() end,              desc = "Previous Todo Comment" },
+            { "<leader>xt", "<cmd>Trouble todo toggle<cr>",                                   desc = "Todo (Trouble)" },
+            { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+            { "<leader>st", "<cmd>TodoTelescope<cr>",                                         desc = "Todo" },
+            { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",                 desc = "Todo/Fix/Fixme" },
+        },
+    },
 }
