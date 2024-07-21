7131035020eb64d3fea10e5336c237980fe0b539
Joseph Sebastian <josephsv96@gmail.com>
2024-07-21 13:17:38 +0200

    nvim config rewrite part 8 : added which key

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,4 +1,5 @@
 return {
+    -- File Explorer
     {
         'nvim-neo-tree/neo-tree.nvim',
         cmd = "Neotree",
@@ -36,13 +37,14 @@ return {
                 end,
                 desc = "Buffer Explorer",
             },
-            {
-                "<leader>se",
-                function()
-                    require("neo-tree.command").execute({ source = "document_symbols", toggle = true })
-                end,
-                desc = "Symbols Explorer",
-            },
+            -- TODO: <leader>s is already used for search, find another key
+            -- {
+            --     "<leader>se",
+            --     function()
+            --         require("neo-tree.command").execute({ source = "document_symbols", toggle = true })
+            --     end,
+            --     desc = "Symbols Explorer",
+            -- },
         },
         deactivate = function()
             vim.cmd([[Neotree close]])
@@ -141,6 +143,77 @@ return {
             --         end
             --     end,
             -- })
+        end,
+    },
+
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts_extend = { "spec" },
+        opts = {
+            spec = {
+                {
+                    mode = { "n", "v" },
+                    { "<leader>c", group = "[C]ode" },
+                    { "<leader>d", group = "[D]ocument" },
+                    { "<leader>r", group = "[R]ename" },
+                    { "<leader>s", group = "[S]earch" },
+                    { "<leader>w", group = "[W]orkspace" },
+                    { "<leader>t", group = "[T]oggle" },
+                    { "<leader>h", group = "Git [H]unk", mode = { 'n', 'v' } },
+
+                    -- TODO: broken
+                    -- { "<leader><tab>", group = "[T]abs" },
+                    -- { "<leader>f", group = "file/find" },
+                    -- { "<leader>g", group = "git" },
+                    -- { "<leader>gh", group = "hunks" },
+                    -- { "<leader>q", group = "quit/session" },
+                    -- { "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
+                    -- { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+                    -- { "[", group = "prev" },
+                    -- { "]", group = "next" },
+                    -- { "g", group = "goto" },
+                    -- { "gs", group = "surround" },
+                    -- { "z", group = "fold" },
+                    {
+                        "<leader>b",
+                        group = "buffer",
+                        expand = function()
+                            return require("which-key.extras").expand.buf()
+                        end,
+                    },
+                    {
+                        "<leader>w",
+                        group = "windows",
+                        proxy = "<c-w>",
+                        expand = function()
+                            return require("which-key.extras").expand.win()
+                        end,
+                    },
+                    -- TODO: broken
+                    -- better descriptions
+                    -- { "gx", desc = "Open with system app" },
+                },
+            },
+        },
+        keys = {
+--            {
+--                "<leader>?",
+--                function()
+--                    require("which-key").show({ global = false })
+--                end,
+--                desc = "Buffer Keymaps (which-key)",
+--            },
+            {
+                "<c-w><space>",
+                function()
+                    require("which-key").show({ keys = "<c-w>", loop = true })
+                end,
+                desc = "Window Hydra Mode (which-key)",
+            },
+        },
+        config = function(_, opts)
+            require("which-key").setup(opts)
         end,
     }
 }
