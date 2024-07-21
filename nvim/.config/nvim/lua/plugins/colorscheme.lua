b21e677e243bb2f623526ef8fdc840ddb89c6fb1
Joseph Sebastian <josephsv96@gmail.com>
2024-07-21 16:52:53 +0200

    nvim config rewrite part 13: fix neo-tree issue

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,12 @@
 return {
-
-  -- Default colorscheme
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "storm",
-      transparent = true,
-    },
-    config = function(_, opts)
-      require("tokyonight").load(opts)
-    end,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "storm",
+    transparent = true,
   },
-
+  config = function(_, opts)
+    require("tokyonight").load(opts)
+  end,
 }
