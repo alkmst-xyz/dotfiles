ca7879d5ce7111a8dc64b45adc48784369dae0e7
Joseph Sebastian <josephsv96@gmail.com>
2024-08-23 11:05:00 +0200

    (fish) added fnm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,7 +10,7 @@ fish_add_path $HOME/.local/bin/
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
 # pnpm
-set -gx PNPM_HOME "/home/joseph/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
