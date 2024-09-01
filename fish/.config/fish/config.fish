ef02bba1330458e7531a08deb9b268d927264459
Joseph Sebastian <josephsv96@gmail.com>
2024-09-01 10:57:35 +0200

    (fish) pnpm completions, pnpm config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -41,3 +41,10 @@ set -gx EDITOR nvim
 starship init fish | source
 
 # end
+
+# pnpm
+set -gx PNPM_HOME "/home/joseph/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
