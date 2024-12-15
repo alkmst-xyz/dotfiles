ac7a14ab2399e57b4b3618e4f572c34f72583d22
Joseph Sebastian <josephsv96@gmail.com>
2024-12-15 17:40:52 +0100

    (tmux) remove catppuccin install script

--- tmux/.config/tmux/install-catppuccin.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin
--- tmux/.config/tmux/install-tpm.sh
+++ tmux/.config/tmux/install-tpm.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
